a1293e22d4182f5c001f9eeb104388f37e9adc4b
vzhuhfaka <ostanin20.06.a@gmail.com>
2024-09-26 23:12:52 +0300

    Реализована функция is_prime(n)

--- src/lab2/rsa.py
+++ src/lab2/rsa.py
@@ -12,8 +12,11 @@ def is_prime(n: int) -> bool:
     >>> is_prime(8)
     False
     """
-    # PUT YOUR CODE HERE
-    pass
+
+    for i in range(2, n):
+        if n % i == 0:
+            return False
+    return True
 
 
 def gcd(a: int, b: int) -> int:
