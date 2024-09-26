459ad07e51ba1b620f9b088f2c8834f1834a9a29
vzhuhfaka <ostanin20.06.a@gmail.com>
2024-09-26 22:46:09 +0300

    Реализован encrypt и decrypt шифра Виженера

--- src/lab2/vigenre.py
+++ src/lab2/vigenre.py
@@ -1,3 +1,13 @@
+from pydoc import plain
+
+
+def fill_str(string, lenght):
+    a = lenght / len(string)
+    res = string * int(a)
+    a = lenght - len(res)
+    return res + string[0:a]
+
+
 def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     """
     Encrypts plaintext using a Vigenere cipher.
@@ -9,9 +19,32 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+
+    alphabet = 'qwertyuioplkjhgfdsazxcvbnm'
+    for symbol_index in range(0, len(plaintext)):
+        symbol = plaintext[symbol_index]
+        unicode_index = ord(symbol)
+        if symbol in alphabet or symbol in alphabet.upper():
+            if symbol in alphabet:
+                shift = ord(fill_str(keyword, len(plaintext))[symbol_index]) - 97
+                if unicode_index + shift > 122:
+                    unicode_index = unicode_index + shift - 26
+                else:
+                    unicode_index += shift
+                ciphertext += chr(unicode_index)
+            else:
+                shift = ord(fill_str(keyword, len(plaintext))[symbol_index]) - 65
+                if unicode_index + shift > 90:
+                    unicode_index = unicode_index + shift - 26
+                else:
+                    unicode_index += shift
+                ciphertext += chr(unicode_index)
+        else:
+            ciphertext += chr(unicode_index)
+
     return ciphertext
 
+print(encrypt_vigenere('Hello world!', 'dnj'))
 
 def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     """
@@ -24,5 +57,26 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+
+    alphabet = 'qwertyuioplkjhgfdsazxcvbnm'
+    for symbol_index in range(0, len(ciphertext)):
+        symbol = ciphertext[symbol_index]
+        unicode_index = ord(symbol)
+        if symbol in alphabet or symbol in alphabet.upper():
+            if symbol in alphabet:
+                shift = ord(fill_str(keyword, len(ciphertext))[symbol_index]) - 97
+                if unicode_index - shift < 97:
+                    unicode_index = unicode_index - shift + 26
+                else:
+                    unicode_index -= shift
+                plaintext += chr(unicode_index)
+            else:
+                shift = ord(fill_str(keyword, len(ciphertext))[symbol_index]) - 65
+                if unicode_index - shift < 65:
+                    unicode_index = unicode_index - shift + 26
+                else:
+                    unicode_index -= shift
+                plaintext += chr(unicode_index)
+        else:
+            plaintext += chr(unicode_index)
     return plaintext
