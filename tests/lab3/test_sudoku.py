5c3e0775eaebddc39addb6bbfb9ef4d203656d73
vzhuhfaka <ostanin20.06.a@gmail.com>
2024-10-01 18:16:57 +0300

    Написаны тесты к функции group в sudoku.py

--- tests/lab3/test_sudoku.py
+++ tests/lab3/test_sudoku.py
@@ -5,4 +5,3 @@ class SudokuTestCase(unittest.TestCase):
 
     def test_sudoku_group(self):
         self.assertEqual(group([1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16], 4), [[1, 2, 3, 4], [5, 6, 7, 8], [9, 10, 11, 12], [13, 14, 15, 16]])
-
